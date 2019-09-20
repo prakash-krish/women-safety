672fcbec0699045ff9e21be8469685de6ae13f26
prakash1605 <54100252+prakash1605@users.noreply.github.com>
2019-09-19 22:33:42 -1000

    file contains R language program

---
+++ model_svm.R
@@ -0,0 +1,13 @@
+library(help="datasets")
+data("iris")
+head(iris)
+View("iris")
+library(ggplot2)
+qplot(Petal.Length,Petal.Width,data = iris)
+#to specify diff color
+qplot(Petal.Length,Petal.Width,data = iris,color=Species)
+#library number for support vector machine SVM
+library(e1071)
+model_svm=svm(Species~.,data=iris)
+summary(model_svm)
+plot(model_svm,data = iris,Petal.Width~Petal.Length,slice=list(Sepal.Width=3,Sepal.Length=4))
